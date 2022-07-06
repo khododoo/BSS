d4d1dc5b8580ddf04fe63b93baa33605b906c771
khododoo <yjma96022@naver.com>
2022-07-06 11:51:13 +0900

    first commit

---
+++ src/test/java/com/example/bss/BssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
